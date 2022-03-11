d8f52e7c639c743b5c6a6867ee1bdc0fc2a8b2f4
riju <riju4350@gmail.com>
2022-03-11 22:49:53 +0600

    little change

--- js/app.js
+++ js/app.js
@@ -57,7 +57,6 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
-  // console.log(post);
   const image = post.image;
   const div = document.createElement("article");
   div.classList.add("post");
